f435ed60b43e6343710c0fc95d54c78f73e2a7b3
jacki890 <jacki890@yahoo.com>
2011-02-14 15:01:31 -0500

    just the puts at this point

---
+++ beer_on_the_wall.rb
@@ -0,0 +1,6 @@
+beer_num = 99
+
+puts beer_num.to_s + ' bottles of beer on the wall'
+puts beer_num.to_s + ' bottles of beer.'
+puts 'Take one down, pass it around.'
+puts beer_num.to_s + ' bottles of beer on the wall!'
