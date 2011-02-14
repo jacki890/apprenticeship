ef4812de95fbc37162f216ba151bdef09bdbbb0f
jacki890 <jacki890@yahoo.com>
2011-02-14 15:40:42 -0500

    working program

--- beer_on_the_wall.rb
+++ beer_on_the_wall.rb
@@ -1,6 +1,11 @@
 beer_num = 99
 
-puts beer_num.to_s + ' bottles of beer on the wall'
-puts beer_num.to_s + ' bottles of beer.'
-puts 'Take one down, pass it around.'
-puts beer_num.to_s + ' bottles of beer on the wall!'
+while beer_num > 0
+  beer_num -= 1
+    puts beer_num.to_s + ' bottles of beer on the wall'
+    puts beer_num.to_s + ' bottles of beer.'
+    puts 'Take one down, pass it around.'
+    puts beer_num.to_s + ' bottles of beer on the wall!'
+    puts ''
+end
+
