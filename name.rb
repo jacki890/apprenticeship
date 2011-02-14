6aba86a7dc9f75f8e341275a5fcf42836fd71b82
jacki890 <jacki890@yahoo.com>
2011-02-14 14:40:49 -0500

    added the OR operator

--- name.rb
+++ name.rb
@@ -2,10 +2,6 @@ puts 'Hello there, and what\'s your name?'
 name = gets.chomp
 puts 'Hello, ' + name + '.'
 
-if name == 'Jacki'
+if name == 'Jacki' || name == 'Mike'
   puts 'What a lovely name!'
-else
-  if name == 'Mike'
-    puts 'What a lovely name!'
-  end
 end
