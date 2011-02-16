eb00c61c28eddd10fb7176d4b0a02ed0cc9d1687
jacki890 <jacki890@yahoo.com>
2011-02-16 17:22:12 -0500

    bye 3 times in a row works now

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,25 @@
-you_say = 'whatever you want'
+puts 'Say something to your grandma.'
 
-until you_say == 'BYE'
-  puts 'Say something to your grandma.'
+you_say = 'whatever you want'
+bye = 0
+
+while bye < 3
   you_say = gets.chomp
-  if you_say != you_say.upcase
-    puts 'WHAT WAS THAT?!? SPEAK UP, SONNY!'
-    puts
+
+  if you_say == 'BYE'
+     bye = (bye + 1)
+
+     if bye == 3
+       puts 'BYE, SONNY. NICE CHATTING WITH YOU!'
+     else
+       puts 'Oh, don\'t go, Sonny.'
+  end
+
   elsif you_say == you_say.upcase
-    puts 'Well, no. Not since ' + (rand(1950-1930)+1930).to_s + '! Cheeky!!'
-    puts
+    puts 'Well, no. Not since ' + (rand(21) + 1930).to_s + '! Cheeky!!'
+    bye = 0
+  else 
+    puts 'WHAT WAS THAT?!? SPEAK UP, SONNY!'
+    bye = 0
   end
 end
